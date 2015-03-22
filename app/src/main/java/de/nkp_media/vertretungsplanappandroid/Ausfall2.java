cbd14ffe3c19aab1e96d7f02131fa9f111f42297
Niklas Schöllhorn <schoellhorn.niklas@gmail.com>
2015-03-22 10:59:30 +0100

    Implement new parser and add a sample feed under res/sample_vertretungsplan.xml

---
+++ app/src/main/java/de/nkp_media/vertretungsplanappandroid/Ausfall2.java
@@ -0,0 +1,79 @@
+package de.nkp_media.vertretungsplanappandroid;
+
+import java.util.Date;
+
+public class Ausfall2 {
+
+    int stunde;
+    private String lehrer;
+    private String vertretung;
+    private Date zieldatum;
+    private String fach;
+    private String zielfach;
+    private String raum;
+    private boolean entfall;
+
+    public int getStunde() {
+        return stunde;
+    }
+
+    public void setStunde(int stunde) {
+        this.stunde = stunde;
+    }
+
+    public String getLehrer() {
+        return lehrer;
+    }
+
+    public void setLehrer(String lehrer) {
+        this.lehrer = lehrer;
+    }
+
+    public String getVertretung() {
+        return vertretung;
+    }
+
+    public void setVertretung(String vertretung) {
+        this.vertretung = vertretung;
+    }
+
+    public Date getZieldatum() {
+        return zieldatum;
+    }
+
+    public void setZieldatum(Date zieldatum) {
+        this.zieldatum = zieldatum;
+    }
+
+    public String getFach() {
+        return fach;
+    }
+
+    public void setFach(String fach) {
+        this.fach = fach;
+    }
+
+    public String getZielfach() {
+        return zielfach;
+    }
+
+    public void setZielfach(String zielfach) {
+        this.zielfach = zielfach;
+    }
+
+    public String getRaum() {
+        return raum;
+    }
+
+    public void setRaum(String raum) {
+        this.raum = raum;
+    }
+
+    public boolean isEntfall() {
+        return entfall;
+    }
+
+    public void setEntfall(boolean entfall) {
+        this.entfall = entfall;
+    }
+}
